441d56b4fd994cba86fd6d9a1bcc42bded076eca
Luv Sharma <luvsharma0009@gmail.com>
2023-07-20 00:53:28 -0400

    Changed apps - using only one app - olapp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students.apps.StudentsConfig',
-    'instructors.apps.InstructorsConfig',
+    'olapp.apps.OlappConfig',
 ]
 
 MIDDLEWARE = [
